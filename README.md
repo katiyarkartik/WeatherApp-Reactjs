2d213879c1394a84bcb7390e66beafd7238988f7
unknown <kartikktyr@gmail.com>
2022-02-12 15:45:33 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+This project was created with the help of openweather api
+that provides us with the various details about weather 
+conditions of places around the world.
+
+openweather api: https://openweathermap.org/api
+
+The project uses various weather data retrieved using 
+openweather api which are:
+
+1.temp
+2.city name
+3.country
+4.minimum temperature
+5.maximum temperature
+6.humidity
+7.pressure
+8.weather information such as clouds, mist ,haze etc.
+
+
---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+
+const api={
+  key:process.env.REACT_APP_API_KEY,
+  base:process.env.REACT_APP_BASE 
+  
+}
+
+function App() {
+  
+
+  const[query,setQuery]=useState('');
+  const[weather,setWeather]=useState({});
+
+  const search=evt=>{
+    if(evt.key==="Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res=>res.json())
+      .then(result=>{
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+
+      }
+        );
+    }
+  }
+  const datebuilder=(d)=>{
+    let months = ["January", "February", "March", "April", "May",
+     "June", "July", "August", "September", "October", "November",
+     "December"]
+    
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+     "Thursday", "Friday", "Saturday"];
+  
+    let day=days[d.getDay()];
+    let date=d.getDate();
+    let month=months[d.getMonth()];
+    let year=d.getFullYear();
+
+    return `${day} , ${date} ${month} ${year}`
+    
+
+  }
+  /*const gettime=(t)=>{
+    let time=t.getHours() + ":" + t.getMinutes();
+    return `${time}`
+  }*/
+  return (
+    <div className={(typeof weather.main !="undefined")?((weather.main.temp>10)?"app warm":"app"):"app normal"}>
+    <main>
+      <div className="search-box">
+        <input
+
+        type="text"
+        className="search-bar"
+        placeholder ="Search City"
+       
+        onChange={e=>setQuery(e.target.value)}
+        value={query}
+        onKeyPress={search}
+        />
+      </div>
+
+      {(typeof weather.main!="undefined")?(
+      <div>
+      
+      <div className="location-box">
+       <div className="location">
+      {weather.name}, {weather.sys.country}
+       </div>
+
+       <div className="date">
+       {datebuilder(new Date())}
+
+       </div>
+      {/* <div className="time">
+         {gettime(new Date())}
+       </div>*/}
+       <div className="weather-box">
+        <div className="temp">
+        {Math.round(weather.main.temp)}°C
+
+        </div> 
+       </div>
+        <div className="weather">
+        <div className="extra">
+         <div>
+                 Pressure:{weather.main.pressure}      
+         </div>
+         <div>
+                 Humidity:{weather.main.humidity} 
+         </div>                               
+                
+        
+        
+         <div>
+                 Min-Temp:{Math.round(weather.main.temp_min)}°C
+         </div>
+         <div>
+                 Max-Temp:{Math.round(weather.main.temp_max)}°C
+         </div>
+         {weather.weather[0].main}
+         </div>
+        </div>
+       </div>
+    </div>
+    ):(<div className="intro">
+    <h1>
+      Weather App
+    </h1>
+    <p>
+      Get current weather details of any city in the world !!!
+    </p>
+    <h2>
+      Enter a valid City name
+    </h2>
+    </div>)}
+    
+    </main>
+     
+    </div>
+  );
+}
+
+export default App;
